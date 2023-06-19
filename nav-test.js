3da009b240c34c72582a11c39043c5cdb98f98b2
ajaysweb <137120465+ajaysweb@users.noreply.github.com>
2023-06-20 08:42:37 +1000

    Add files via upload

---
+++ nav-test.js
@@ -0,0 +1,13 @@
+﻿function toggleMenuOpacity() {
+  var mainNavHolder = document.querySelector('.main-nav-holder');
+  var menuOverlay = document.querySelector('.menu-overlay');
+  
+  if (mainNavHolder.style.opacity === '0') {
+    mainNavHolder.style.opacity = '1';
+    menuOverlay.style.opacity = '1';
+  } else {
+    mainNavHolder.style.opacity = '0';
+    menuOverlay.style.opacity = '0';
+  }
+}
+
